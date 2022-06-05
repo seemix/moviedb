0e8c6e915a0dc64280c77e1048b417d9520db51c
seemix <semikhalchuk@gmail.com>
2022-06-05 13:48:48 +0300

    mdb1 10

--- src/app/modules/movies/movies.module.ts
+++ src/app/modules/movies/movies.module.ts
@@ -3,19 +3,19 @@ import { CommonModule } from '@angular/common';
 import { MoviesComponent } from './components/movies/movies.component';
 import {MoviesRoutingModule} from "./movies-routing.module";
 import { MovieComponent } from './components/movie/movie.component';
-// import {AppModule} from "../../app.module";
+import { PageComponent } from './components/page/page.component';
 
 
 @NgModule({
   declarations: [
     MoviesComponent,
     MovieComponent,
+    PageComponent,
   ],
 
   imports: [
     CommonModule,
     MoviesRoutingModule,
-    // AppModule
   ],
 
 })
